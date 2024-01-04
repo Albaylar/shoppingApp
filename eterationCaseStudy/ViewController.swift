2c118ce200897d1b23cf236af472728a709655a8
Albaylar <furkandenizalbaylar@gmail.com>
2024-01-04 11:02:52 +0300

    Initial Commit

---
+++ eterationCaseStudy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eterationCaseStudy
+//
+//  Created by Furkan Deniz Albaylar on 4.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
